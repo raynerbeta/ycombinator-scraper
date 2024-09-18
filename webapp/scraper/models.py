33502da70f5809f3735db2123a0b2aee3c551184
raynerbeta <33467773+raynerbeta@users.noreply.github.com>
2024-09-18 10:16:06 -0500

    Add Entry class and initial test cases

--- webapp/scraper/models.py
+++ webapp/scraper/models.py
@@ -1,9 +1,32 @@
-from django.db import models
+class Entry:
+    """Class for handling entries."""
 
-# Create your models here.
-class Entry():
     def __init__(self, number, title, points, comments) -> None:
+        """Object initialization."""
         self.number = number
         self.title = title
         self.points = points
         self.comments = comments
+
+    def __repr__(self) -> str:
+        """Custom representation of the class object."""
+        return f"Entry({self.number},{self.title},{self.title},{self.points},{self.comments})"
+
+    def to_dict(self):
+        """Method for yielding a dictionary from the object."""
+        return {
+            "number": self.number,
+            "title": self.title,
+            "points": self.points,
+            "comments": self.comments,
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        """Class method for yielding an Entry object from a dictionary."""
+        return cls(
+            number=data["number"],
+            title=data["title"],
+            points=data["points"],
+            comments=data["comments"],
+        )
--- webapp/scraper/tests.py
+++ webapp/scraper/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Entry
+
+
+# Utilities
+def generate_entries(to_dict=False, size=3):
+    """Utility for faking entries."""
+    entries = []
+    for i in range(size):
+        entry = Entry(i, f"Entry #{i}", i**2, i**3)
+        entries.append(entry.to_dict() if to_dict else entry)
+    return entries
+
 
 # Create your tests here.
+class SessionStorageTest(TestCase):
+    """Class for testing session storage."""
+
+    def setUp(self):
+        """Clear session storage for every test case."""
+        self.client.session.clear()
+
+    def test_session_storage_for_entries(self):
+        """Test session storage to save and retrieve entries."""
+        session = self.client.session
+        session["entries"] = []
+        session.save()
+        self.assertEqual(len(session["entries"]), 0)
+        session["entries"] = generate_entries(True)
+        session.save()
+        self.assertEqual(len(session["entries"]), 3)
+
+
+class ViewTest(TestCase):
+    """Class for testing the main view of the app."""
+
+    def setUp(self):
+        """Clear session storage for every test case."""
+        self.client.session.clear()
+
+    def test_view_with_session_data(self):
+        session = self.client.session
+        self.assertEqual(session.get("entries"), None)
+        response = self.client.get(reverse("entries"))
+        self.assertEqual(len(session.get("entries")), 30)
