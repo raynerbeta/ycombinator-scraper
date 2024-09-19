6ed4ebfd3bef0cb21260a1d3e6e168f2e81c64b2
raynerbeta <33467773+raynerbeta@users.noreply.github.com>
2024-09-19 17:00:41 -0500

    Fix retrieve_entries function

--- webapp/conftest.py
+++ webapp/conftest.py
@@ -1,3 +1,5 @@
+"""File for pytest configuration."""
+
 import pytest
 
 
--- webapp/scraper/views.py
+++ webapp/scraper/views.py
@@ -4,60 +4,76 @@ from bs4 import BeautifulSoup
 from .models import Entry
 from .forms import FilterForm
 
-URL = "https://news.ycombinator.com/"
+BASE_URL = "https://news.ycombinator.com/"
 DESIRED_ENTRIES = 30
 
 
 def retrieve_entries(to_dict=False):
     """Function used to scrape news from specific URL."""
+    # Initialize entries with an empty list
+    entries = []
+    # Initialize current_page, we increment until entries size meets DESIRED_ENTRIES
+    current_page = 1
     # Condition specified for running indefinitely until return is reached
     while True:
-        response = requests.get(URL)
-        html_content = response.content
-        soup = BeautifulSoup(html_content, "html.parser")
-        # Extract all table rows with class "athing"
-        rows = soup.find_all("tr", class_="athing")
-        entries = []
-        for row in rows:
-            # Retrieve the number from span with class "rank" and remove "."
-            number = row.find("span", class_="rank").text.replace(".", "").strip()
-            # Extract the second table data with class "title"
-            td_title = row.find_all("td", class_="title")[1]
-            # Retrieve the title from the first anchor tag
-            title = td_title.find("a").text.strip()
-            # Extract the next table row that contains relevant data
-            # And also extract table data with class "subtext"
-            subtext = row.find_next_sibling("tr").find("td", class_="subtext")
-            # Retrieve points from span with class "score"
-            points = subtext.find("span", class_="score")
-            if points:
-                points = points.text.replace(" points", "").strip()
-            else:
-                points = 0
-            # Retrieve comments from the last anchor tag
-            comments = subtext.find_all("a")[-1].text.strip()
-            if "comment" in comments:
-                # Remove character escape sequence
-                comments = comments.split()[0].replace("\xa0", "")
-            else:
-                comments = 0
-            # Create Entry object
-            entry = Entry(number=number, title=title, points=points, comments=comments)
-            entries.append(entry.to_dict() if to_dict else entry)
-            # Return condition for stopping the external while
-            if len(entries) == DESIRED_ENTRIES:
-                return entries
+        try:
+            # Form request url
+            url = f"{BASE_URL}?p={current_page}"
+            # Request URL with current page
+            response = requests.get(url)
+            # Raise exception if something failed
+            response.raise_for_status()
+            html_content = response.content
+            soup = BeautifulSoup(html_content, "html.parser")
+            # Extract all table rows with class "athing"
+            rows = soup.find_all("tr", class_="athing")
+            for row in rows:
+                # Retrieve the number from span with class "rank" and remove "."
+                number = row.find("span", class_="rank").text.replace(".", "").strip()
+                # Extract the second table data with class "title"
+                td_title = row.find_all("td", class_="title")[1]
+                # Retrieve the title from the first anchor tag
+                title = td_title.find("a").text.strip()
+                # Extract the next table row that contains relevant data
+                # And also extract table data with class "subtext"
+                subtext = row.find_next_sibling("tr").find("td", class_="subtext")
+                # Retrieve points from span with class "score"
+                points = subtext.find("span", class_="score")
+                if points:
+                    points = points.text.replace(" points", "").strip()
+                else:
+                    points = 0
+                # Retrieve comments from the last anchor tag
+                comments = subtext.find_all("a")[-1].text.strip()
+                if "comment" in comments:
+                    # Remove character escape sequence
+                    comments = comments.split()[0].replace("\xa0", "")
+                else:
+                    comments = 0
+                # Create Entry object
+                entry = Entry(
+                    number=number, title=title, points=points, comments=comments
+                )
+                # Append dictionary or object considering to_dict
+                entries.append(entry.to_dict() if to_dict else entry)
+                # Return condition for stopping the external while
+                if len(entries) == DESIRED_ENTRIES:
+                    return entries
+            # Move to the next page
+            current_page += 1
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return []
 
 
 def entries_view(request):
     """Function view for rendering the main page."""
-    # Session storage is used for persisting the entries once retrieved
+    # Session storage is used for persisting the entries after they're retrieved the first time
     session = request.session
-    # Check if entries are already loaded in session
-    if "entries" not in session:
+    # Check if entries aren't saved or differ from DESIRED_ENTRIES
+    if len(session.get("entries", [])) != DESIRED_ENTRIES:
         # If not, they're fetched and saved
         session["entries"] = retrieve_entries(True)
-        session.save()
     # Load entries from session
     entries = [Entry.from_dict(entry) for entry in session["entries"]]
     # Load form
