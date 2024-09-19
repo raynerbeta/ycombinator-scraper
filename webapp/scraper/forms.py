a68a7a618e729d3d85aa9dfaee43f2001383c839
raynerbeta <33467773+raynerbeta@users.noreply.github.com>
2024-09-19 14:51:34 -0500

    Improve template entries.html

--- webapp/scraper/forms.py
+++ webapp/scraper/forms.py
@@ -2,10 +2,12 @@ from django import forms
 
 
 class FilterForm(forms.Form):
+    """Form used for displaying filtering options."""
+
     FILTER_CHOICES = [
         ("", "No filter"),
-        ("filter_1", "Filter 1"),
-        ("filter_2", "Filter 2"),
+        ("filter_1", "Filter 1 (>5 words, comments)"),
+        ("filter_2", "Filter 2 (<=5 words, points)"),
     ]
     filter = forms.ChoiceField(
         choices=FILTER_CHOICES, required=False, label="Filter by"
